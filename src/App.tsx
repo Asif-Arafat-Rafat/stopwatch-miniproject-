d12af1440c24194d151b7f4fde323df370b632b7
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-03-04 15:08:25 +0600

    Stopwatch done+ cc ;

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     
-      {ctype!="stopwatch" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("stopwatch")}>Stopwatch</button>}
+      {ctype!="stopwatch" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("stopwatch")} >Stopwatch</button>}
       {ctype!="timer" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("timer")}>Timer</button>}
       {ctype!="alarm" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("alarm")}>Alarm</button>}
 
--- src/stopwatch.tsx
+++ src/stopwatch.tsx
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react";
 
 function stopwatch(){
     const [ time ,setTime]=useState(0)
-    const [isRunning, setIsRunning] = useState(false);
+    const [isRunning, setIsRunning] = useState("stop");
 
     useEffect(() => {
         let interval:any;
-        if(isRunning){
+        if(isRunning==="start"){
+            console.log("start detected");
             interval=setInterval(() => {
                 setTime((prevTime)=>{
                     return prevTime+10;
                 })
             }, 10);
         }
-        else if(!isRunning){
+        else if(isRunning==="stop"){
+            console.log("stop detected");
             setTime(0);
-            return ()=>clearInterval(interval);
+
         }
-        return () => clearInterval(interval);
+        return ()=>clearInterval(interval);
+
     }, [isRunning]);
  
 
@@ -25,8 +28,8 @@ function stopwatch(){
         <>
             <h1 className="underline text-5xl text-center">Stopwatch</h1>
             <h1 className="text-center text-6xl bg-amber-100">{("0"+Math.floor((time/360000)%60)).slice(-2)}:{("0"+Math.floor((time/60000)%60)).slice(-2)}:{("0"+Math.floor((time/1000)%60)).slice(-2)}:{("0"+Math.floor((time/10)%100)).slice(-2)}</h1>
-            <div className="flex justify-center">{!isRunning&&<button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning(true)}>Start</button>}
-            {isRunning&&<div><button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning(false)}>Stop</button> <button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning(false)}>Pause</button></div>}</div>
+            <div className="flex justify-center">{isRunning!="start"&&<button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning("start")}>Start</button>}
+            {isRunning=="start" &&<div><button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning("stop")}>Stop</button> <button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning("pause")}>Pause</button></div>}</div>
         </>
         )
 }
