f4db1c3c4b7b47e06a80c7832abe3b0b7e821e34
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-03-05 16:49:36 +0600

    stopwatch lap function working

--- src/stopwatch.tsx
+++ src/stopwatch.tsx
@@ -3,7 +3,16 @@ import { useEffect, useState } from "react";
 function stopwatch(){
     const [ time ,setTime]=useState(0)
     const [isRunning, setIsRunning] = useState("stop");
+    const [lap,setLap]=useState<number[]>([])
+    const addLap=()=>{
+        setLap((prevLap:number[])=>{
+            return [...prevLap,time]})
+    }
+    const removelap=(index:number)=>{
+        setLap((prevLap) => prevLap.filter((_, i) => i !== index));
 
+    }
+    
     useEffect(() => {
         let interval:any;
         if(isRunning==="start"){
@@ -20,17 +29,27 @@ function stopwatch(){
 
         }
         return ()=>clearInterval(interval);
-
     }, [isRunning]);
  
-
     return (
-        <>
-            <h1 className="underline text-5xl text-center">Stopwatch</h1>
-            <h1 className="text-center text-6xl bg-amber-100">{("0"+Math.floor((time/360000)%60)).slice(-2)}:{("0"+Math.floor((time/60000)%60)).slice(-2)}:{("0"+Math.floor((time/1000)%60)).slice(-2)}:{("0"+Math.floor((time/10)%100)).slice(-2)}</h1>
-            <div className="flex justify-center">{isRunning!="start"&&<button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning("start")}>Start</button>}
-            {isRunning=="start" &&<div><button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning("stop")}>Stop</button> <button className="bg-green-400 w-24 h-12 rounded-lg text-2xl m-2" onClick={()=>setIsRunning("pause")}>Pause</button></div>}</div>
-        </>
+        <><div className="flex justify-center">
+            <div className="border-6 w-2xl border-y-zinc-400 border-x-yellow-900 rounded-4xl p-10">
+                    <h1 className=" pb-10 text-white text-5xl text-center">Stopwatch</h1>
+                    <div className="flex justify-center items-center gap-3">
+                        <h1 className="text-center text-6xl text-white pb-10">{("0"+Math.floor((time/360000)%60)).slice(-2)}:{("0"+Math.floor((time/60000)%60)).slice(-2)}:{("0"+Math.floor((time/1000)%60)).slice(-2)}</h1><h1 className="text-white text-4xl">{("0"+Math.floor((time/10)%100)).slice(-2)}</h1>
+                    </div>
+                    <div className="flex justify-center">{isRunning!="start"&&<button className="bg-green-400 rounded-lg text-2xl m-2 px-10 py-3" onClick={()=>setIsRunning("start")}>Start</button>}
+                    {isRunning=="start" &&<div> <button className="bg-green-400 rounded-lg text-2xl m-2 px-10 py-3" onClick={()=>setIsRunning("pause")}>Pause</button><button className="bg-green-400 rounded-lg text-2xl m-2 px-10 py-3" onClick={()=>setIsRunning("stop")}>Stop</button> <button onClick={addLap} className="bg-green-400 rounded-lg text-2xl m-2 px-10 py-3">Lap</button>
+                    </div>}
+                    {isRunning=="pause" &&<div><button className="bg-green-400 rounded-lg text-2xl m-2 px-10 py-3" onClick={()=>setIsRunning("stop")}>Stop</button><button onClick={addLap} className="bg-green-400 rounded-lg text-2xl m-2 px-10 py-3">Lap</button></div>}
+                    </div>
+            <div >
+                {lap.map((l,index)=>{
+                    return <div className="flex justify-center items-end gap-3" key={index}><p className="text-4xl text-white">{("0"+Math.floor(l/360000)%60).slice(-2)}:{("0"+Math.floor(l/60000)%60).slice(-2)}:{("0"+Math.floor(l/1000)%60).slice(-2)}</p><p className="text-2xl text-white">{("0"+Math.floor(l/10)%100).slice(-2)}</p> <button className="text-white" onClick={()=>removelap(index)}>X</button></div>
+                })}
+            </div>
+        </div>
+        </div></>
         )
 }
 export default stopwatch;   
