d6185be06123940d422dc822a154da681e544f05
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-03-08 20:58:24 +0600

    Alarm now shows time

--- src/alarm.tsx
+++ src/alarm.tsx
@@ -1,14 +1,25 @@
-function alarm() {
+import { useEffect, useState } from "react";
+
+function Alarm() {
     const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const [hours,setHours] = useState(date.getHours());
+    const [minutes,setMinutes] = useState(date.getMinutes());
+    const [seconds,setSeconds] = useState(date.getSeconds());
+    useEffect(()=>{
+        let interval=setInterval(()=>{
+            const date = new Date();
+            setHours( date.getHours() );
+            setMinutes(date.getMinutes());
+            setSeconds(date.getSeconds());
+        },1000)
+        return ()=>clearInterval(interval);
+    },[])
     return(
         <>
             <div className="flex justify-center">
-                <h1 className="text-white text-4xl">{hours%12}:{minutes}:{seconds}{ (hours>12) ? " PM":" AM"}</h1>
+                <h1 className="text-white text-4xl">{(hours%12)==0? 12 : ("0"+(hours%12)).slice(-2)}:{("0"+minutes).slice(-2)}:{("0"+seconds).slice(-2)}{ (hours>=12) ? " PM":" AM"}</h1>
             </div>
         </>
     )
-}
-export default alarm    
+} 
+export default Alarm    
