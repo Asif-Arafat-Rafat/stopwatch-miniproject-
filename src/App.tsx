79ab522529253d51f214f469ca652379f27802f3
Asif-Arafat-Rafat <asif.arafat.rafat@gmail.com>
2025-03-09 14:56:01 +0600

    Stopwatch and timer Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import Stopwatch from "./stopwatch";
 import Timer from "./timer";
-import Alarm from "./alarm";
- 
+  
 function App() {
   const [ctype, setCtype] = useState("stopwatch");
   return (
     <>
-        {ctype!="stopwatch" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-2xl px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("stopwatch")} >Stopwatch</button>}
-        {ctype!="timer" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-2xl px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("timer")}>Timer</button>}
-        {ctype!="alarm" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 text-2xl rounded-lg px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("alarm")}>Alarm</button>}
+        <div className="flex justify-center items-center flex-col gap-10">
+          {ctype!="stopwatch" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-2xl px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("stopwatch")} >Stopwatch</button>}
+          {ctype!="timer" &&<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-2xl px-10 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>setCtype("timer")}>Timer</button>}
+        </div>
         {ctype==="stopwatch" && <Stopwatch/>}
         {ctype==="timer" && <Timer/>}
-        {ctype==="alarm" && <Alarm/>}
     </>
   )
 }
